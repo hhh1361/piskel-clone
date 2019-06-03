9aca2b8e6cec29beef180b2046a2cd9af0873485
hhh1361 <nikita.turlyko@gmail.com>
2019-06-03 22:45:33 +0300

    feat: add frame container

--- codejam-dom-pallete/index.js
+++ codejam-dom-pallete/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 /* eslint-disable no-console */
 /* eslint-disable no-plusplus */
 const pen = document.getElementById('pen');
@@ -8,6 +9,8 @@ const canvas = document.getElementById('myCanvas');
 const context = canvas.getContext('2d');
 const mouse = { x: 0, y: 0 };
 const frames = document.getElementById('frames');
+const FPS = 3;
+const container = document.getElementById('container');
 
 
 let draw = false;
@@ -18,7 +21,8 @@ const thickness = 20;
 const canvasSize = 32;
 let array;
 let previewArray;
-let i = 1;
+let num = 1;
+let num2 = 1;
 
 
 // Pen :hover
@@ -123,23 +127,98 @@ plus.addEventListener('click', () => {
   const createCanvas = document.createElement('canvas');
   createCanvas.width = 224;
   createCanvas.height = 224;
-  createCanvas.className = `frame _${i++}`;
+  createCanvas.className = `frame _${num++}`;
   const deleteCurrent = document.getElementById('current');
   deleteCurrent.removeAttribute('id');
   createCanvas.id = 'current';
 
   frames.appendChild(createCanvas);
   context.clearRect(0, 0, canvas.width, canvas.height);
+
+
+  let collection = document.getElementsByClassName('frame');
+  let frameAray = Array.from(collection);
+  if (array.length > 4) {
+    frames.style.transform = `translateY(-${(frameAray.length - 3) * 241}px)`;
+  }
+  for (let i = 0; i < frameAray.length; i++) {
+    frameAray[i].onclick = () => {
+      container.removeChild(frameAray[i]);
+      collection = document.getElementsByClassName('box');
+      frameAray = Array.from(collection);
+      if (frameAray.length > 4) {
+        frames.style.transform = `translateY(-${(frameAray.length - 3) * 241}px)`;
+      }
+    };
+
+    frameAray[i].onmouseenter = () => {
+    // delete :hover
+      const del = document.createElement('div');
+      del.id = `delete_${num2}`;
+      del.style.zIndex = '999';
+      del.innerHTML = 'Delete';
+      del.style.fontSize = '32pt';
+      del.style.fontFamily = "'Titillium Web', sans-serif";
+      del.style.width = '223px';
+      del.style.marginLeft = '5px';
+      del.style.height = '50px';
+      del.style.color = 'white';
+      del.style.backgroundColor = '#B62525';
+      del.style.borderRadius = '100% 100% 0 0';
+      del.style.boxShadow = 'inset 0px 0px 15px 0px rgb(209, 209, 191)';
+      del.style.textAlign = 'center';
+      del.style.lineHeight = '50px';
+      del.style.position = 'absolute';
+      del.style.transform = 'translateY(179px)';
+      del.style.transition = 'all, 1s';
+      del.onclick = (event) => {
+        frames.removeChild(event.currentTarget.nextSibling.nextSibling);
+      };
+      frames.insertBefore(del, frameAray[i]);
+
+      // copy :hover
+      const copy = document.createElement('div');
+      copy.id = `copy_${num2}`;
+      num2++;
+      copy.style.zIndex = '999';
+      copy.innerHTML = 'Copy';
+      copy.style.fontSize = '32pt';
+      copy.style.fontFamily = "'Titillium Web', sans-serif";
+      copy.style.width = '223px';
+      copy.style.marginLeft = '5px';
+      copy.style.height = '50px';
+      copy.style.color = 'white';
+      copy.style.backgroundColor = '#27E109';
+      copy.style.borderRadius = '0 0 100% 100%';
+      copy.style.boxShadow = 'inset 0px 0px 40px 0px rgb(209, 209, 191)';
+      copy.style.textAlign = 'center';
+      copy.style.lineHeight = '50px';
+      copy.style.position = 'absolute';
+      copy.style.transform = 'translateY(5px)';
+      copy.onclick = (event) => {
+        const copy1 = event.currentTarget.nextSibling.cloneNode(true);
+        frames.insertBefore(copy1, event.currentTarget.nextSibling);
+      };
+      frames.insertBefore(copy, frameAray[i]);
+    };
+    frameAray[i].onmouseleave = () => {
+      const del = document.getElementById(`delete_${num2 - 1}`);
+      setTimeout(() => {
+        frames.removeChild(del);
+      }, 500);
+      const copy = document.getElementById(`copy_${num2 - 1}`);
+      setTimeout(() => {
+        frames.removeChild(copy);
+      }, 500);
+    };
+  }
 });
 
-const preview = document.getElementById('preview');
-preview.addEventListener('click', () => {
-  const ctx = preview.getContext('2d');
-  ctx.drawImage(previewArray[0], 0, 0);
-  setTimeout(() => {
-    ctx.drawImage(previewArray[1], 0, 0);
-  }, 1000);
-  setTimeout(() => {
-    ctx.drawImage(previewArray[2], 0, 0);
-  }, 2000);
-});
+// const preview = document.getElementById('preview');
+// preview.addEventListener('click', () => {
+//  const ctx = preview.getContext('2d');
+//  ctx.drawImage(previewArray[0], 0, 0);
+//  let timer = setInterval(() => {
+//
+//  }, 1000/FPS);
+// });
