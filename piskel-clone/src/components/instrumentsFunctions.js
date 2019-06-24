d18151bb3b9b550dc99b0619928eb3f4e07994c0
hhh1361 <nikita.turlyko@gmail.com>
2019-06-24 23:35:22 +0300

    add color section events

--- piskel-clone/src/components/instrumentsFunctions.js
+++ piskel-clone/src/components/instrumentsFunctions.js
@@ -4,6 +4,8 @@
 const instrument = {
   state: '?',
   penSize: 1,
+  currentColor: '?',
+  previousColor: '?',
 };
 
 const instrumentsFunction = function instrumentsFunction() {
@@ -31,7 +33,7 @@ const penSizeFunction = function penSizeFunction() {
       }
       e.target.className = `${e.target.className}Current`;
       e.target.children[0].className = `${e.target.children[0].className}Current`;
-      instrument.penSize = e.target.children[0].innerHTML;
+      instrument.penSize = +e.target.children[0].innerHTML;
     } else if (e.target.className === 'square') {
       for (let i = 0; i < 4; i++) {
         thickness.children[i].className = 'penSize';
@@ -39,11 +41,53 @@ const penSizeFunction = function penSizeFunction() {
       }
       e.target.parentNode.className = `${e.target.parentNode.className}Current`;
       e.target.className = `${e.target.className}Current`;
-      instrument.penSize = e.target.innerHTML;
+      instrument.penSize = +e.target.innerHTML;
     }
     global.console.log(instrument);
   });
 };
+
+const currentColor = document.getElementById('currentColor');
+const previousColor = document.getElementById('previousColor');
+const swapColor = document.getElementById('swapColor');
+
+
+currentColor.addEventListener('click', (e) => {
+  if (currentColor.state !== 'activate') {
+    currentColor.children[0].style.display = 'flex';
+    currentColor.state = 'activate';
+  } else {
+    currentColor.children[0].style.display = 'none';
+    currentColor.state = 'deactivate';
+    instrument.previousColor = instrument.currentColor;
+    instrument.currentColor = getComputedStyle(e.target).backgroundColor;
+    currentColor.style.backgroundColor = instrument.currentColor;
+    previousColor.style.backgroundColor = instrument.previousColor;
+    console.log(instrument);
+  }
+});
+
+previousColor.addEventListener('click', (e) => {
+  if (previousColor.state !== 'activate') {
+    previousColor.children[0].style.display = 'flex';
+    previousColor.state = 'activate';
+  } else {
+    previousColor.children[0].style.display = 'none';
+    previousColor.state = 'deactivate';
+    instrument.previousColor = getComputedStyle(e.target).backgroundColor;
+    previousColor.style.backgroundColor = instrument.previousColor;
+    console.log(instrument);
+  }
+});
+
+swapColor.addEventListener('click', () => {
+  const temp = instrument.currentColor;
+  instrument.currentColor = instrument.previousColor;
+  instrument.previousColor = temp;
+  currentColor.style.backgroundColor = instrument.currentColor;
+  previousColor.style.backgroundColor = instrument.previousColor;
+});
+
 export {
   instrument, instrumentsFunction, penSizeFunction,
 };
