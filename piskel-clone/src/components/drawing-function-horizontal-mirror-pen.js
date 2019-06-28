4495828719ee3e662bdf1df7f7259d39139e736f
hhh1361 <nikita.turlyko@gmail.com>
2019-06-28 20:00:58 +0300

    add horizontal mirror pen function

---
+++ piskel-clone/src/components/drawing-function-horizontal-mirror-pen.js
@@ -0,0 +1,81 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable max-len */
+import { instrument } from './instruments-functions';
+import { canvas } from './canvas-creation-functions';
+
+const drawingFunctionHorizontalMirrorPen = function drawingFunctionHorizontalMirrorPen() {
+  const mouse = { x: 0, y: 0 };
+  let draw = false;
+  const mainCanvas = document.getElementById('mainCanvas');
+  canvas.canvas = mainCanvas;
+  const context = mainCanvas.getContext('2d');
+
+  // mousedown event - start drawing
+  mainCanvas.addEventListener('mousedown', (e) => {
+    mouse.x = e.pageX - mainCanvas.offsetLeft;
+    mouse.y = e.pageY - mainCanvas.offsetTop;
+    if (instrument.state === 'horizontalMirrorPen' && instrument.currentColor !== '?') {
+      draw = true;
+      global.console.log(mouse.x, mouse.y);
+      context.fillStyle = instrument.currentColor;
+
+      for (let i = 0; i < instrument.penSize; i++) {
+        for (let j = 0; j < instrument.penSize; j++) {
+          canvas.array[Math.floor(mouse.y / 5) + i][Math.floor(mouse.x / 5) + j] = { color: instrument.currentColor };
+        }
+      }
+
+      for (let i = 0; i < instrument.penSize; i++) {
+        for (let j = 0; j < instrument.penSize; j++) {
+          canvas.array[Math.ceil((canvas.array.length * 5 - mouse.y) / 5) - 1 - i][Math.floor(mouse.x / 5) + j] = { color: instrument.currentColor };
+        }
+      }
+
+
+      context.fillRect(
+        Math.floor(mouse.x / 5) * 5, Math.floor(mouse.y / 5) * 5, instrument.penSize * 5, instrument.penSize * 5,
+      );
+
+      context.fillRect(
+        Math.floor(mouse.x / 5) * 5, (canvas.array.length - Math.floor(mouse.y / 5)) * 5 - instrument.penSize * 5, instrument.penSize * 5, instrument.penSize * 5,
+      );
+    }
+  });
+
+  // mousemove event - continue drawing
+  mainCanvas.addEventListener('mousemove', (e) => {
+    if (draw === true && instrument.state === 'horizontalMirrorPen') {
+      mouse.x = e.pageX - mainCanvas.offsetLeft;
+      mouse.y = e.pageY - mainCanvas.offsetTop;
+
+      for (let i = 0; i < instrument.penSize; i++) {
+        for (let j = 0; j < instrument.penSize; j++) {
+          canvas.array[Math.floor(mouse.y / 5) + i][Math.floor(mouse.x / 5) + j] = { color: instrument.currentColor };
+        }
+      }
+
+      for (let i = 0; i < instrument.penSize; i++) {
+        for (let j = 0; j < instrument.penSize; j++) {
+          canvas.array[Math.ceil((canvas.array.length * 5 - mouse.y) / 5) - 1 - i][Math.floor(mouse.x / 5) + j] = { color: instrument.currentColor };
+        }
+      }
+
+
+      context.fillRect(
+        Math.floor(mouse.x / 5) * 5, Math.floor(mouse.y / 5) * 5, instrument.penSize * 5, instrument.penSize * 5,
+      );
+
+      context.fillRect(
+        Math.floor(mouse.x / 5) * 5, (canvas.array.length - Math.floor(mouse.y / 5)) * 5 - instrument.penSize * 5, instrument.penSize * 5, instrument.penSize * 5,
+      );
+    }
+    if (instrument.state === 'horizontalMirrorPen') {
+      document.onmouseup = () => {
+        draw = false;
+        document.onmouseup = null;
+      };
+    }
+  });
+};
+
+export default drawingFunctionHorizontalMirrorPen;
--- piskel-clone/src/index.js
+++ piskel-clone/src/index.js
@@ -5,6 +5,7 @@ import fullscreenFunction from './components/fullscreen-function';
 import canvasInformationFunction from './components/canvas-information-function';
 import drawingFunctionPen from './components/drawing-function-pen';
 import drawingFunctionVerticalMirrorPen from './components/drawing-function-vertical-mirror-pen';
+import drawingFunctionHorizontalMirrorPen from './components/drawing-function-horizontal-mirror-pen';
 import drawingFunctionEraser from './components/drawing-function-eraser';
 import drawingFunctionPixels from './components/drawing-function-paint-all-pixels-of-the-same-color';
 
@@ -17,5 +18,6 @@ fullscreenFunction();
 canvasInformationFunction();
 drawingFunctionPen();
 drawingFunctionVerticalMirrorPen();
+drawingFunctionHorizontalMirrorPen();
 drawingFunctionEraser();
 drawingFunctionPixels();
